23e9f12b81f6d9fc82a302b60ec795624c907b13
陈宏淑 <1842634125@qq.com>
2021-03-12 09:39:15 +0800

    更新hello文件

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,5 +1,8 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("陈宏淑长点心吧");
+        System.out.println("陈宏淑长点心吧");
+        System.out.println("陈宏淑长点心吧");
+        System.out.println("陈宏淑长点心吧");
     }
 }
