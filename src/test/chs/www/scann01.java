e2c44586fa8a033a0dbcc9d043640f22d67a56a4
陈宏淑 <1842634125@qq.com>
2021-03-26 07:19:14 +0800

    提交commit到本地

---
+++ src/test/chs/www/scann01.java
@@ -0,0 +1,12 @@
+package test.chs.www;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class scann01 {
+    public static void main(String[] args) {
+        int[] nums={23,45,64,88};
+        int a=88;
+        System.out.println(Arrays.binarySearch(nums,a));
+    }
+}
