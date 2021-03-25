1ab4a1ac375e9f62ed9809eb20b976ca6074d5c6
陈宏淑 <1842634125@qq.com>
2021-03-26 07:30:02 +0800

    修改scanner

--- src/test/chs/www/scann01.java
+++ src/test/chs/www/scann01.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class scann01 {
     public static void main(String[] args) {
-        int[] nums={23,45,64,88};
+        int[] nums={23,64,88};
         int a=88;
         System.out.println(Arrays.binarySearch(nums,a));
     }
